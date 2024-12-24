88c1a921ef3189d66ca5c50c06d8ae05882193ed
Tuan <101162316+TuanVu2504@users.noreply.github.com>
2024-12-24 10:15:17 +0700

    Update config.js

--- pages/pac/config.js
+++ pages/pac/config.js
@@ -1,9 +1,13 @@
 function FindProxyForURL(url, host) {
-    if ( 
-      shExpMatch(url, "*whatsapp*")
-    ){
-        return "PROXY 127.0.0.1:60000";
-    } else {
-        return "DIRECT";
+    var domainList = [
+        "*whatsapp*",
+        "*outlook*"
+    ];
+    for (var i = 0; i < domainList.length; i++) {
+        if ( shExpMatch(url, domainList[i])){
+            return "PROXY 127.0.0.1:60000";
+        }
     }
+    
+    return "DIRECT";
 }
