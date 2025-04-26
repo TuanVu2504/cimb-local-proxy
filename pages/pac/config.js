dbde01e7a0a820c3a1a3435dd414c3bbd5fd18bc
Tuan <101162316+TuanVu2504@users.noreply.github.com>
2025-04-26 12:56:53 +0700

    Update config.js

--- pages/pac/config.js
+++ pages/pac/config.js
@@ -15,7 +15,7 @@ function FindProxyForURL(url, host) {
         "*fbcdn.net*",
         "*reddit*",
         "*dev.to*",
-        "*api7*",
+        "*api7*", "*jenkins*",
         "*enterprisedb*", "*stackoverflow*",
         "*onecompiler*", "*groovyide*",
         "*cloudflare*", "*google-apis*", "*google-analytics*",
